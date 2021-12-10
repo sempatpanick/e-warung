72af4ba7964129762f61349724d4de4576b0ee20
Steven Rowantio <stevenrowantio@gmail.com>
2021-12-10 10:52:02 +0700

    update user provider for recommended product result and add product

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -2,6 +2,7 @@ import 'package:ewarung/data/api/api_service.dart';
 import 'package:ewarung/data/model/geeneral_result.dart';
 import 'package:ewarung/data/model/detail_product_user_result.dart';
 import 'package:ewarung/data/model/products_user_result.dart';
+import 'package:ewarung/data/model/recommended_product_result.dart';
 import 'package:ewarung/utils/get_connection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -9,6 +10,40 @@ import 'package:http/http.dart' as http;
 class UserProvider extends ChangeNotifier {
   final ApiService apiService = ApiService();
   final _getConnection = GetConnection();
+
+  Future<RecommendedProductResult> fetchRecommendedProduct(String idUser) async {
+    try {
+      notifyListeners();
+      final connection = await _getConnection.getConnection();
+      if (connection) {
+        notifyListeners();
+        return await apiService.getRecommendedProduct(http.Client(), idUser);
+      } else {
+        notifyListeners();
+        return RecommendedProductResult(status: false, message: "Tidak ada koneksi internet");
+      }
+    } catch (e) {
+      notifyListeners();
+      return RecommendedProductResult(status: false, message: "Failed to get recommended product, $e");
+    }
+  }
+
+  Future<GeneralResult> fetchAddProductUser(String idUser, String idProduct, String name, String description, int price, int stock, String image) async {
+    try {
+      notifyListeners();
+      final connection = await _getConnection.getConnection();
+      if (connection) {
+        notifyListeners();
+        return await apiService.addProduct(http.Client(), idUser, idProduct, name, description, price, stock, image);
+      } else {
+        notifyListeners();
+        return GeneralResult(status: false, message: "Tidak ada koneksi internet");
+      }
+    } catch (e) {
+      notifyListeners();
+      return GeneralResult(status: false, message: "Failed to add product, $e");
+    }
+  }
 
   Future<ProductsUserResult> fetchProductsUser(String idUser) async {
     try {

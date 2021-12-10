b9e277618ffc6313b785f34eab6b23d9baecc103
Steven Rowantio <stevenrowantio@gmail.com>
2021-12-10 11:03:12 +0700

    upadate fab to add product by scanner and manually

--- lib/widgets/expandable_fab.dart
+++ lib/widgets/expandable_fab.dart
@@ -1,5 +1,16 @@
 import 'package:ewarung/common/styles.dart';
+import 'package:ewarung/data/model/form_product.dart';
+import 'package:ewarung/data/model/recommended_product_result.dart';
+import 'package:ewarung/provider/cart_provider.dart';
+import 'package:ewarung/provider/user_provider.dart';
+import 'package:ewarung/provider/utils_provider.dart';
+import 'package:ewarung/widgets/custom_alert_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:provider/provider.dart';
+
+import 'custom_notification_snackbar.dart';
 
 class ExpandableFab extends StatefulWidget {
   const ExpandableFab({Key? key}) : super(key: key);
@@ -9,6 +20,8 @@ class ExpandableFab extends StatefulWidget {
 }
 
 class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProviderStateMixin {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController _textIdProductController = TextEditingController();
   bool _isOpened = false;
   late AnimationController _animationController;
   late Animation<Color?> _buttonColor;
@@ -51,12 +64,6 @@ class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProvider
     super.initState();
   }
 
-  @override
-  dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
   animate() {
     if (!_isOpened) {
       _animationController.forward();
@@ -66,59 +73,96 @@ class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProvider
     _isOpened = !_isOpened;
   }
 
-  Widget addProduct() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        _isOpened
-          ? Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                color: textColorBlue,
-                border: Border.all(color: textColorGrey),
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Text(
-                "Add Product",
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(color: textColorWhite, fontSize: 16.0, fontWeight: FontWeight.w600),
-              ),
-            )
-          : Container(),
-        const FloatingActionButton(
-          backgroundColor: primaryColor,
-          onPressed: null,
-          tooltip: 'Add Product',
-          child: Icon(Icons.document_scanner_outlined),
-        ),
-      ],
-    );
-  }
-
-  Widget addProductManual() {
+  Widget addProductBarcode(UserProvider userProvider, CartProvider cartProvider, UtilsProvider utilsProvider) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         _isOpened
             ? Container(
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                  color: textColorBlue,
-                  border: Border.all(color: textColorGrey),
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                ),
-                child: Text(
-                  "Add Product Manually",
-                  style: Theme.of(context).textTheme.subtitle1!.copyWith(color: textColorWhite, fontSize: 16.0, fontWeight: FontWeight.w600),
-                ),
-              )
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            color: textColorBlue,
+            border: Border.all(color: textColorGrey),
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Text(
+            "Add Product",
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(color: textColorWhite, fontSize: 16.0, fontWeight: FontWeight.w600),
+          ),
+        )
             : Container(),
-        const FloatingActionButton(
+        FloatingActionButton(
           backgroundColor: primaryColor,
-          onPressed: null,
+          onPressed: () {
+            scanBarcodeNormal(userProvider, cartProvider, utilsProvider);
+          },
+          tooltip: 'Add Product',
+          child: const Icon(Icons.document_scanner_outlined),
+        ),
+      ],
+    );
+  }
+
+  Widget addProductManual(UserProvider userProvider, CartProvider cartProvider, UtilsProvider utilsProvider) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        _isOpened
+            ? Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            color: textColorBlue,
+            border: Border.all(color: textColorGrey),
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Text(
+            "Add Product Manually",
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(color: textColorWhite, fontSize: 16.0, fontWeight: FontWeight.w600),
+          ),
+        )
+            : Container(),
+        FloatingActionButton(
+          backgroundColor: primaryColor,
+          onPressed: () {
+            showDialog(context: context, builder: (context) {
+              return Form(
+                key: formKey,
+                child: CustomAlertDialog(
+                    title: Center(
+                      child: Text(
+                        "Add New Product",
+                        style: Theme.of(context).textTheme.subtitle1!.copyWith(color: textColorBlue, fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          controller: _textIdProductController,
+                          decoration: const InputDecoration(
+                              labelText: "ID Product",
+                              border: OutlineInputBorder()
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                      ],
+                    ),
+                    submit: () {
+                      final isValid = formKey.currentState!.validate();
+
+                      if (isValid) {
+                        formKey.currentState!.save();
+                        processAdd(userProvider, cartProvider, utilsProvider, _textIdProductController.text, "manual");
+                      }
+                    }
+                ),
+              );
+            });
+          },
           tooltip: 'Add stock',
-          child: Icon(Icons.note_add_outlined),
+          child: const Icon(Icons.note_add_outlined),
         ),
       ],
     );
@@ -143,6 +187,10 @@ class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProvider
 
   @override
   Widget build(BuildContext context) {
+    UserProvider userProvider = Provider.of<UserProvider>(context);
+    CartProvider cartProvider = Provider.of<CartProvider>(context);
+    UtilsProvider utilsProvider = Provider.of<UtilsProvider>(context);
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
@@ -152,7 +200,7 @@ class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProvider
             _translateButton.value * 2.0,
             0.0,
           ),
-          child: addProduct(),
+          child: addProductBarcode(userProvider, cartProvider, utilsProvider),
         ),
         Transform(
           transform: Matrix4.translationValues(
@@ -160,10 +208,77 @@ class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProvider
             _translateButton.value,
             0.0,
           ),
-          child: addProductManual(),
+          child: addProductManual(userProvider, cartProvider, utilsProvider),
         ),
         toggle(),
       ],
     );
+  }
+
+  Future<void> scanBarcodeNormal(UserProvider userProvider, CartProvider cartProvider, UtilsProvider utilsProvider) async {
+    String barcodeScanRes;
+
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.BARCODE);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    if (barcodeScanRes != "-1") {
+      processAdd(userProvider, cartProvider, utilsProvider, barcodeScanRes, "scan");
+    }
+  }
+
+  processAdd(UserProvider userProvider, CartProvider cartProvider, UtilsProvider utilsProvider, String idProduct, String type) async {
+    if (!cartProvider.listProducts.any((element) => element.idProduk == idProduct)) {
+      try {
+        final Future<RecommendedProductResult> response = userProvider.fetchRecommendedProduct(idProduct);
+
+        response.then((value) {
+          RecommendedProduct recommendedProduct = RecommendedProduct(id: idProduct, nama: "", keterangan: "", harga: "", gambar: "");
+          FormProduct formProduct = FormProduct(title: "Add Product", type: "add_product");
+          if (type == "manual") {
+            setState(() {
+              Navigator.pop(context);
+            });
+          }
+          if(value.status) {
+            setState(() {
+              utilsProvider.setIsFormInputProduct(true);
+              utilsProvider.setRecommendedProduct(value.data ?? recommendedProduct);
+              utilsProvider.setFormProduct(formProduct);
+            });
+            CustomNotificationSnackbar(context: context, message: "There are recommendations for the same product");
+          } else {
+            setState(() {
+              utilsProvider.setIsFormInputProduct(true);
+              utilsProvider.setRecommendedProduct(recommendedProduct);
+              utilsProvider.setFormProduct(formProduct);
+            });
+          }
+        });
+      } catch (e) {
+        setState(() {
+          utilsProvider.setIsFormInputProduct(false);
+        });
+
+        CustomNotificationSnackbar(context: context, message: "Error : $e");
+      }
+    } else {
+      setState(() {
+        utilsProvider.setIsFormInputProduct(false);
+      });
+
+      CustomNotificationSnackbar(context: context, message: "Produk tersebut sudah terdaftar di toko anda");
+    }
+  }
+
+  @override
+  dispose() {
+    _textIdProductController.dispose();
+    _animationController.dispose();
+    super.dispose();
   }
 }

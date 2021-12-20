b7f94ec5c22c726a2ed30fe6e3b5be4a7978bc28
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2021-12-20 22:22:24 +0700

    Add header result by json

--- server/api/user/login.php
+++ server/api/user/login.php
@@ -1,36 +1,39 @@
 <?php
+    header('Content-Type: application/json; charset=utf-8');
     if (isset($_POST['email']) && isset($_POST['password'])) {
         require_once("../koneksi.php");
-
+        
         $email = $_POST['email'];
         $pass = $_POST['password'];
-
+        
         $query = mysqli_query($koneksi, "SELECT * FROM users WHERE email='$email' AND password='$pass'");
         if(mysqli_num_rows($query) > 0){
             $myArray = null;
             while ($row = $query->fetch_array(MYSQLI_ASSOC)) {
                 $myArray = $row;
             }
-
+            
             $data = array(
                 'status' => true,
                 'message' => "Login success",
                 'data' => $myArray
             );
-
+            
             echo json_encode($data);
         } else {
             $data = array(
                 'status' => false,
                 'message' => "Username/password salah",
+                'data' => null
             );
-
+            
             echo json_encode($data);
         }
     } else {
         $data = array(
             'status' => false,
             'message' => "invalid query parameters",
+            'data' => null
         );
         
         echo json_encode($data);
